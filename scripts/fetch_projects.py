8a585d4e6e776074911606d1f0126e50ea05d9af
v0ila8085 <v0ila8085@outlook.com>
2020-07-12 11:32:51 +0200

    modified projects

--- scripts/fetch_projects.py
+++ scripts/fetch_projects.py
@@ -146,9 +146,9 @@ for repo in public_repos:
     DATA_JSON[repo_full_name]["watchers"] = DATA_JSON[repo_full_name]["watchers"]["totalCount"]
 
 # Save to _data directory
-file_path = PATH_TO_DATA + "/" + "projects.json"
-with open(file_path, "w+") as f:
-    json.dump(DATA_JSON, f)
+# file_path = PATH_TO_DATA + "/" + "projects.json"
+# with open(file_path, "w+") as f:
+#     json.dump(DATA_JSON, f)
 print("LOG: Saved to", file_path)
 
 
