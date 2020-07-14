28721aa209bb526533f295355b6b2e38eb66ba11
v0ila8085 <v0ila8085@outlook.com>
2020-07-14 19:01:23 +0200

    modified projects

--- scripts/fetch_papers.py
+++ scripts/fetch_papers.py
@@ -9,7 +9,9 @@ papers = glob.glob(f"{PATH_TO_DATA}/*.json")
 papers_json = {}
 
 for paper in papers:
+
     papers_json[paper['title']]  = paper
+    papers_json[paper['title']]['authors'] = " ".join([i['name'] for i in paper['authors']])
     papers_json[paper['title']]['localFile'] = f"{PATH_TO_DATA}/papers/{paper['title']}"
 
 with open(f"{PATH_TO_DATA}/papers.json","w")as f:
