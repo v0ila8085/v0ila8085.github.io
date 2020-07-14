3852031baae92a5864867ff902f6b6ec111b33c0
v0ila8085 <v0ila8085@outlook.com>
2020-07-14 08:46:08 +0200

    modified projects

---
+++ scripts/fetch_papers.py
@@ -0,0 +1,18 @@
+import os
+import json
+import glob
+
+PATH_TO_DATA = "_data"
+
+papers = glob.glob(f"{PATH_TO_DATA}/*.json")
+
+papers_json = {}
+
+for paper in papers:
+    papers_json[paper['title']]  = paper
+    papers_json[paper['title']]['localFile'] = f"{PATH_TO_DATA}/papers/{paper['title']}"
+
+with open(f"{PATH_TO_DATA}/papers.json")as f:
+    f.write(json.dumps(paper))
+
+
