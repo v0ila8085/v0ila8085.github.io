1d51eaf8b9446add2eb1fc6a700dfd1cac544476
v0ila8085 <v0ila8085@outlook.com>
2020-07-14 20:21:23 +0200

    modified projects

--- scripts/fetch_papers.py
+++ scripts/fetch_papers.py
@@ -13,7 +13,7 @@ for paper in papers:
     with open(paper,'r') as p:
         paper = json.loads(p.read())
         papers_json[paper['title']]  = paper
-        papers_json[paper['title']]['localFile'] = f"{PATH_TO_DATA}/papers/{paper['title']}.pdf"
+        papers_json[paper['title']]['localFile'] = f"ok.pdf"
 
 with open(f"{PATH_TO_DATA}/papers.json","w")as f:
     f.write(json.dumps(paper))
