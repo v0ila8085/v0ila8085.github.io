61aa660f1c611a920a5abc7eac324a724afab679
v0ila8085 <v0ila8085@outlook.com>
2020-07-14 20:40:43 +0200

    modified projects

--- scripts/fetch_papers.py
+++ scripts/fetch_papers.py
@@ -6,12 +6,13 @@ PATH_TO_DATA = "_data"
 
 papers = glob.glob(f"papers/*.json")
 
-papers_json = {}
+papers_json = []
 
 for paper in papers:
     with open(paper,'r') as p:
         paper = json.loads(p.read())
-        papers_json[paper['title']]  = paper
+        # papers_json[paper['title']]  = paper
+        papers_json.push(paper)
 
 with open(f"{PATH_TO_DATA}/papers.json","w")as f:
     f.write(json.dumps(papers_json))
